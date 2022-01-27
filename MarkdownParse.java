e9bf79423f01b237b46b5f47da30d30c5d83c17c
bcli12 <97566583+bcli12@users.noreply.github.com>
2022-01-26 18:30:42 -0800

    testing getLinks method

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,6 +15,9 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if(nextOpenBracket == -1 || nextCloseBracket == -1|| openParen == -1 || closeParen == -1){
+                break;
+            }
             if(nextCloseBracket + 1 != openParen){
                 currentIndex = closeParen + 1;
                 continue;
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,28 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+    @Test
+    public void tester() throws IOException {
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://something.com");
+        expected.add("some-page.html");
+        List<String> list = List.of("empty-file.md", "empty-link.md", "image-file.md", "test-file.md");
+        for (String string : list) {
+            Path fileName = Path.of(string);
+	        String contents = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(contents);
+            assertEquals(expected, links);
+        }
+    }
+
 }
