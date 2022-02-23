e55f3b6db66f9ecade7005f2d2c78ec9d46c917e
bcli12 <97566583+bcli12@users.noreply.github.com>
2022-02-22 17:34:16 -0800

    added snippet tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -16,10 +16,9 @@ public class MarkdownParseTest {
     @Test
     public void tester() throws IOException {
         ArrayList<String> expected = new ArrayList<>();
-        //expected.add("https://something.com");
-        //expected.add("some-page.html");
-        //expected.add("");
-        List<String> list = List.of("empty-link.md");
+        expected.add("https://something.com");
+        expected.add("some-page.html");
+        List<String> list = List.of("test-file.md");
         for (String string : list) {
             Path fileName = Path.of(string);
 	        String contents = Files.readString(fileName);
@@ -35,6 +34,52 @@ public class MarkdownParseTest {
         //expected.add("some-page.html");
         //expected.add("");
         List<String> list = List.of("not-a-link.md");
+        for (String string : list) {
+            Path fileName = Path.of(string);
+	        String contents = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(contents);
+            assertEquals(expected, links);
+        }
+    }
+
+    @Test
+    public void testSnippet1() throws IOException {
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("url.com");
+        expected.add("`google.com");
+        List<String> list = List.of("snippet1.md");
+        for (String string : list) {
+            Path fileName = Path.of(string);
+	        String contents = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(contents);
+            assertEquals(expected, links);
+        }
+    }
+
+    @Test
+    public void testSnippet2() throws IOException {
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("a.com");
+        expected.add("b.com");
+        expected.add("a.com(())");
+        expected.add("example.com");
+        List<String> list = List.of("snippet2.md");
+        for (String string : list) {
+            Path fileName = Path.of(string);
+	        String contents = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(contents);
+            assertEquals(expected, links);
+        }
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://www.twitter.com");
+        expected.add("https://ucsd-cse15l-w22.github.io/");
+        expected.add("github.com");
+        expected.add("https://cse.ucsd.edu/");
+        List<String> list = List.of("snippet3.md");
         for (String string : list) {
             Path fileName = Path.of(string);
 	        String contents = Files.readString(fileName);
